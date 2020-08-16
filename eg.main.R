e156750ddd97ef91bf3c3edd62cec70e6857d96d
funstatpackages <liwangresearchgroup@gmail.com>
2020-08-16 13:15:54 -0400

    Update_0816

--- eg.main.R
+++ eg.main.R
@@ -9,7 +9,7 @@ library(seastests)
 library(segmented)
 library(tscount)
 
-date.update = as.Date("2020-08-14")
+date.update = as.Date("2020-08-15")
 
 ######################################################################
 # Step 0. Read in Data
